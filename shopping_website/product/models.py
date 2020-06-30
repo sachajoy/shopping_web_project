93566be844cb5e1586f5cb57a0797c358328b80f
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-06-30 12:16:53 +0530

    added def __str__ method in all product models

--- shopping_website/product/models.py
+++ shopping_website/product/models.py
@@ -5,17 +5,18 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     sub_category = models.ForeignKey("self", on_delete = models.CASCADE, db_constraint=False, null=True)
 
+    def __str__(self):
+        return "{}".format(self.name)
 class Product(models.Model):
     name= models.CharField(max_length= 100)
-    category=models.ForeignKey(Category, on_delete=models.CASCADE) 
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField()
     img_path = models.TextField(default="")
     price =models.FloatField()
     is_active=models.BooleanField(default=False)
     quantity=models.FloatField()
 
-
-
-#many to one relation between product and category
+    def __str__(self):
+        return "{}, {}".format(self.name, self.category)
 
 
