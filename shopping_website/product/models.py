f31ed15ff5a8d3776db1fc66c266b120d897099d
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-06-30 12:00:18 +0530

    changed model in product for pid remove and parent to sub_category in category class

--- shopping_website/product/models.py
+++ shopping_website/product/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    parent = models.ForeignKey("self", on_delete = models.CASCADE, db_constraint=False, null=True)
+    sub_category = models.ForeignKey("self", on_delete = models.CASCADE, db_constraint=False, null=True)
 
 class Product(models.Model):
-    pId =models.CharField(primary_key=True, max_length=100)
     name= models.CharField(max_length= 100)
     category=models.ForeignKey(Category, on_delete=models.CASCADE) 
     description = models.TextField()
